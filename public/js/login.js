9e21a3dcedf953a468be6db56d7d6bd8f2abe6a2
minusInfinite <ashley@minusinfinite.net>
2021-09-13 01:25:15 +0930

    corrected signup from email to name

--- public/js/login.js
+++ public/js/login.js
@@ -25,7 +25,7 @@ const loginForm = async (event) => {
 const signupForm = async (event) => {
     event.preventDefault()
 
-    const name = document.querySelector("#email-signup").value.trim()
+    const name = document.querySelector("#name-signup").value.trim()
     const email = document.querySelector("#email-signup").value.trim()
     const password = document.querySelector("#password-signup").value.trim()
 
